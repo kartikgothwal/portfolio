e222f50ffb603793757f777e804bdaf6a96f8446
Kartik Gothwal <kartikgothwal28@gmail.com>
2023-11-07 18:17:53 +0530

    Remove .env from cache

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,8 @@ import { useGLTF, OrbitControls, Preload } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 // import CanvasLoader from  '../../../public/desktop_pc/scene.gltf'
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("../../../public/desktop_pc/scene.gltf");
+  // const computer = useGLTF("../../../public/desktop_pc/scene.gltf");
+  const computer = useGLTF("./public/desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={3.15} groundColor="black" />
